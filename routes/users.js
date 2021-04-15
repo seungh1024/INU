64f528faf01f775538e11a7d01720bcde2eceec6
seungh1024 <seungh1024@naver.com>
2021-04-15 08:19:59 +0000

    users.js 에서 아이디,닉네임 유뮤 판별 추가, 가입을 위한 함수 추가

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,9 @@ const router = express.Router();
 //요청 방식을 바꾸기로함
 //post 방식을 이용하고 안의 데이터를 구분하여 받기로 해서 수정함
 router.post('/',async(req,res,next)=>{
-  if(req.body.Method=='Login'){
+  console.log(req.body);
+
+  if(req.body.Method=='Login') {
     try{
       const user = await User.findOne({
         attributes:["Nick"],
@@ -24,20 +26,67 @@ router.post('/',async(req,res,next)=>{
     }
   }
 
+  else if(req.body.Method=='ID Check') {
+    try{
+      const user = await User.findOne({
+        where:{
+          ID:req.body.ID,
+        }
+      })
+      // res.status(200).json(user);
+    }catch(err){
+      console.error(err);
+      next(err);
+    }
+    if (user == "") { res.send("True"); }
+    else { res.send("False"); }
+  }
+
+  else if(req.body.Method=='Nick Check') {
+    try{
+      const user = await User.findOne({
+        where:{
+          Nick:req.body.Nick,
+        }
+      })
+      // res.status(200).json(user);
+    }catch(err){
+      console.error(err);
+      next(err);
+    }
+    if (user == "") { res.send("True"); }
+    else { res.send("False"); }
+  }
+
+  else if(req.body.Method=='Regist') {
+    try{
+      const user = await User.create({
+        ID : req.body.ID,
+        PW : req.body.PW,
+        Who : req.body.Who,
+        Nick : req.body.Nick,
+      });
+      console.log(user);
+      res.json(user);
+    }catch(err){
+      console.error(err);
+      next(err);
+    }
+  }
 })
 
 
-router.delete('/:id/delete',async(req,res,next)=>{
-  try{
-    const users = await User.destroy({
-      where:{id:req.params.id}
-    });
-    res.json(users);
-  }catch(err){
-    console.error(err);
-    next(err);
-  }
-});
+// router.delete('/:id/delete',async(req,res,next)=>{
+//   try{
+//     const users = await User.destroy({
+//       where:{id:req.params.id}
+//     });
+//     res.json(users);
+//   }catch(err){
+//     console.error(err);
+//     next(err);
+//   }
+// });
 
 
 

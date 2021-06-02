07be17ed3cf6c1d1b7e70d44751f30ed0c2ac2b6
seungh1024 <seungh1024@naver.com>
2021-06-02 04:52:49 +0000

    응답요청 없는 것과 필요없는 코드 제거

--- routes/stores.js
+++ routes/stores.js
@@ -8,7 +8,8 @@ const router = express.Router();
 router.route('/')// stores/로 get방식일 때
  .get(async (req, res, next) => {
     try {
-      const stores = await Store.findAll({});
+      const stores = await Store.findAll({
+      });
       res.json(stores);
     } catch (err) {
       console.error(err);
@@ -83,7 +84,7 @@ router.post('/',async(req,res,next)=>{
 router.patch('/',async(req,res,next)=>{
   if(req.body.Method == "switch"){
     try{
-      const store = await Store.update(
+      var store = await Store.update(
         {
           Status:req.body.Status,
         },
@@ -91,6 +92,8 @@ router.patch('/',async(req,res,next)=>{
           where:{Store_code:req.body.Store_code},
         }
       )
+      console.log(store);
+      res.status(200).send();
     }catch(err){
       console.error(err);
       next(err);
@@ -98,32 +101,5 @@ router.patch('/',async(req,res,next)=>{
   }
   
 })
-
-
-// router.patch('/:store_code/status',async(req,res,next)=>{
-//     //가게코드와 해당 테이블 번호의 모든 status값을 0,1로 변환
-//     var cnt = 0;
-//     var change = await Store.findOne({
-//         where:{store_code:req.params.store_code},
-//     });
-//     if(change.status == 0){
-//         cnt =1;
-//     }else{
-//         cnt = 0;
-//     }
-//     try{
-//         const result = await Store.update({
-//             status:cnt,
-//             
-//         },{
-//             where:{store_code:req.params.store_code  },
-//         });
-//         res.json(result);
-//     }catch(err){
-//         console.error(err);
-//         next(err);
-//     }
-// 
-// })
 
   module.exports = router;
